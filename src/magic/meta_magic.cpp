0aa9a4c2458aa018a45cf38a9044280354798595
amazzetta <aaron.mazzetta@gmail.com>
2024-05-06 19:16:18 +0200

    add initial magic files

---
+++ src/magic/meta_magic.cpp
@@ -0,0 +1,10 @@
+/**
+*   DO NOT TOUCH!!
+*
+*   This file will be changed after compiling for the first time.
+*   This way we only need to calculate our magic numbers once.
+*
+*/
+
+#include "magic/magic.h"
+const bool magic::initialized_magics = false;
